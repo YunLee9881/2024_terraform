3198993928e65e68a77e0c17e469f03154d258f6
YunLee <yunjuwon0211@gmail.com>
2024-03-17 15:49:47 +0900

    first commit

---
+++ vpc/README.md
@@ -0,0 +1,24 @@
+## 소개
+vpc, subnet, subnetgroup, routetable, natgateway
+
+### vpc
+name, cidr_blocks
+
+### subnet
+subnet_name, tags(k8s), auto ipv4 enable
+
+### subnetGroup
+name, elasticache, DocumentDB
+
+### routetable
+name_tags
+
+### natgateway
+per az, 
+
+
+## 사용 방법
+
+1. vpc, subnet, routetable, subnetgroup 이름 당일 과제에 맞게 수정
+2. subnet 대역 당일 과제에 맞게 수정
+3. 2az, 3az 비사용 파일 확장자 뒤에 아무 문자 추가
---
+++ vpc/vpc.tf
@@ -0,0 +1,42 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.6.0"
+
+  name = var.vpc_name
+  cidr = var.cidr_block
+
+  azs                 = var.azs
+  public_subnet_names = var.public_subnet_names
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  public_subnets          = var.public_subnets_cidr
+  public_route_table_tags = var.public_route_table_tags
+  map_public_ip_on_launch = true
+
+  private_subnet_names = var.private_subnet_names
+  private_subnets      = var.private_subnets_cidr
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  private_route_table_tags = var.private_route_table_tags
+
+  database_subnet_names      = var.database_subnet_names
+  database_subnets           = var.database_subnets_cidr
+  database_route_table_tags  = var.database_route_table_tags
+  database_subnet_group_name = var.database_subnet_group_name
+
+  elasticache_subnet_group_name = var.elasticache_subnet_group_name
+
+
+
+  enable_nat_gateway     = true
+  one_nat_gateway_per_az = true
+
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+}
